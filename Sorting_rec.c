e25a3726a06f668023b3f727101889265e947ce9
saksham2186 <saksham2186@gmail.com>
2026-01-30 13:51:07 +0530

    Add files via upload

---
+++ Sorting_rec.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+void display(int arr[],int n)
+{
+    for (int i=0;i<n;i++)
+    {
+        printf("%d",arr[i]);
+    }
+    printf("\n");
+}
+void bubble_sort_rec(int arr[] ,int n)
+{
+    if(n==1)
+    {
+        return;
+    }
+    for(int j= 0; j<=n-2;j++)
+    {
+        if(arr[j]>arr[j+1])
+        {
+            int temp = arr[j];
+            arr[j] = arr[j+1];
+            arr[j+1]=temp;
+        }
+    }
+    bubble_sort_rec(arr,n-1);
+}
+int main()
+{
+    int arr[]={50, 40, 30, 20, 10};
+    int n=sizeof(arr) / sizeof(arr[0]);
+    display(arr,n);
+    // bubble_sort_rec(arr,n);
+    bubble_sort_rec(arr,n);
+    display(arr,n);
+}
